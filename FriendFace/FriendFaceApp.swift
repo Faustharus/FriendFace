3bf04d77c65890885d631d85698d74861f3fbe62
Faustharus <chailloleau-damien@outlook.fr>
2023-12-21 12:11:03 +0100

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Damien Chailloleau on 21/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
